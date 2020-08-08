2ea978081fe1d129d8fda405ecfa4f2560806f6f
Yuwei Wan <yuweiwan2015@gmail.com>
2020-08-08 06:48:29 -0400

    Update evaluate.py

--- model/evaluate.py
+++ model/evaluate.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
-@Author: jby
-@Date: 2020-07-13 11:00:51
-@LastEditTime: 2020-07-18 15:41:43
-@LastEditors: Please set LastEditors
-@Description: Evaluate the loss in the dev set.
-@FilePath: /JD_project_2/baseline/model/evaluate.py
-'''
+
 import os
 import sys
 import pathlib
